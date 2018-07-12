00b61bad037289873d192e08d77d45990d8410b8
muschellij2 <muschellij2@gmail.com>
2018-07-12 17:07:43 -0400

    Addding to the lungreg function.

--- R/lungreg_function.R
+++ R/lungreg_function.R
@@ -1,18 +1,25 @@
-reg_helper = function(moving_mask,
-                      fixed_mask, value = 1,
-                      moving = NULL,
-                      verbose = FALSE,
-                      outprefix = NULL,
-                      add_prefix = "_left_",
-                      interpolator = "linear") {
+reg_helper = function(
+  moving_mask,
+  fixed_mask,
+  value = 1,
+  moving = NULL,
+  verbose = FALSE,
+  outprefix = NULL,
+  add_prefix = "_left",
+  mask_interpolator = "nearestNeighbor",
+  interpolator = "linear",
+  save_memory = TRUE
+) {
 
   if (is.null(outprefix)) {
     outprefix = tempfile()
   }
 
   # Just the left lobe
-  moving_mask = moving_mask == value | moving_mask == 3
-  fixed_mask = fixed_mask == value  | fixed_mask == 3
+  # moving_mask = moving_mask == value | moving_mask == 3
+  # fixed_mask = fixed_mask == value  | fixed_mask == 3
+  moving_mask = moving_mask == value
+  fixed_mask = fixed_mask == value
 
   # if any voxels are in left mask
   if (sum(moving_mask) > 0) {
@@ -20,15 +27,36 @@ reg_helper = function(moving_mask,
       fixed = fixed_mask,
       moving = moving_mask,
       typeofTransform = "SyN",
-      verbose = verbose,
-      outprefix = paste0(outprefix, add_prefix))
+      verbose = verbose)
+
+    #compose the transforms
+    outfile = paste0(outprefix, add_prefix, ".nii.gz")
+    composed = antsApplyTransforms(
+      fixed = fixed_mask,
+      moving = moving_mask,
+      transformlist = reg$fwdtransforms,
+      compose = tempfile())
+    file.copy(composed, outfile, overwrite = TRUE)
+
+    transformed_mask = antsApplyTransforms(
+      fixed = fixed_mask,
+      moving = moving_mask,
+      transformlist = outfile,
+      interpolator = mask_interpolator)
+
+    reg$warpedmovout = NULL
+    reg$warpedfixout = NULL
+    for (i in 1:10) {
+      gc();
+    }
+
     if (!is.null(moving)) {
       moving = check_ants(moving)
       image = moving * moving_mask
-      transformed= antsApplyTransforms(
+      transformed = antsApplyTransforms(
         fixed = fixed_mask,
         moving = image,
-        transformlist = reg$fwdtransforms,
+        transformlist = outfile,
         interpolator = interpolator)
     } else {
       transformed = NULL
@@ -37,11 +65,19 @@ reg_helper = function(moving_mask,
   } else {
     return(NULL)
   }
-  L = list(reg_out = reg)
+  L = list(composed_fwdtransforms = outfile)
   L$transformed = transformed
-  L$masked_image = image
-  L$moving_mask = moving_mask
-  L$fixed_mask = fixed_mask
+  L$transformed_mask = transformed_mask
+  if (!save_memory) {
+    L$masked_image = image
+    L$moving_mask = moving_mask
+    L$fixed_mask = fixed_mask
+  } else {
+    rm(list = c("image", "moving_mask", "fixed_mask"))
+    for (i in 1:10) {
+      gc();
+    }
+  }
   return(L)
 }
 
@@ -58,17 +94,24 @@ reg_helper = function(moving_mask,
 #' @param verbose Print diagnostic messages
 #' @param interpolator interpolator used to apply transformation to image,
 #' passed to \code{\link{antsApplyTransforms}}
+#' @param mask_interpolator interpolator used to apply transformation to moving mask,
+#' passed to \code{\link{antsApplyTransforms}}
+#' @param save_memory do garbage collection and not save intermediate
+#' files in registration.
 #'
 #' @return A list of registrations, transformed images, each for
 #' left and right separately
 #' @importFrom ANTsRCore antsRegistration antsApplyTransforms
 #' @export
-register_lung_mask = function(moving_mask,
-                              fixed_mask,
-                              moving = NULL,
-                              outprefix = NULL,
-                              verbose = FALSE,
-                              interpolator = "linear"
+register_lung_mask = function(
+  moving_mask,
+  fixed_mask,
+  moving = NULL,
+  outprefix = NULL,
+  verbose = FALSE,
+  mask_interpolator = "nearestNeighbor",
+  interpolator = "linear",
+  save_memory = TRUE
 ) {
   # verbose = FALSE;
   moving_mask = check_ants(moving_mask)
@@ -82,29 +125,33 @@ register_lung_mask = function(moving_mask,
     moving = check_ants(moving)
   }
 
-  reg_left = reg_helper(
-    moving_mask,
-    fixed_mask,
-    value = 1,
-    moving = moving,
-    verbose = verbose,
-    outprefix = outprefix,
-    add_prefix = "_left_",
-    interpolator = interpolator)
 
-  reg_right = reg_helper(
-    moving_mask,
-    fixed_mask,
-    value = 2,
+  args = list(
+    moving_mask = moving_mask,
+    fixed_mask = fixed_mask,
     moving = moving,
     verbose = verbose,
     outprefix = outprefix,
-    add_prefix = "_right_",
-    interpolator = interpolator)
+    mask_interpolator = mask_interpolator,
+    interpolator = interpolator,
+    save_memory = save_memory)
+
+  # run left
+  args$add_prefix = "_left"
+  args$value = 1
+  reg_left = do.call("reg_helper", args = args)
+
+  # run right
+  args$add_prefix = "_right"
+  args$value = 2
+  reg_right = do.call("reg_helper", args = args)
+
+
   res = list()
   res$reg_left = reg_left
   res$reg_right = reg_right
   res$interpolator = interpolator
+  res$mask_interpolator = mask_interpolator
   res$outprefix = outprefix
   return(res)
 }
