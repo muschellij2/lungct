2967f6c5ab6cf27ba80629c47595407e8514f337
Sarah <sarah.m.ryan@ucdenver.edu>
2019-02-05 20:29:00 -0700

    labelClusters wasn't working right
    It was giving the background a non-zero value, which messed up the left/right segmentation. It is now fixed. I also added a couple other things to make it run better.

--- R/segment_lung_lr.R
+++ R/segment_lung_lr.R
@@ -7,52 +7,79 @@
 #' @param verbose Print diagnostic messages.
 #'
 #' @return Lung mask, with left and right designation, of original CT scan.
-#' Right lung has values = 1, left lung has values = 2, left/right lung overlap
-#' (i.e. couldn't distinguish the left and right lungs) has values = 3, non-lung = 0.
+#' Right lung has values = 1, left lung has values = 2, non-lung = 0.
 #' @importFrom ANTsR maskImage
-#' @importFrom ANTsRCore iMath labelClusters
+#' @importFrom ANTsRCore iMath labelClusters makeImage
 #' @export
 segment_lung_lr = function(img, lthresh = -300, verbose = TRUE){
 
-  orig_img = check_ants(img)
-  img = antsImageClone(orig_img)
-  if (verbose) {
-    message("# Resampling Image to 1x1x1")
-  }
-  img = resampleImage(img, c(1,1,1))
+    orig_img = check_ants(img)
+    img = antsImageClone(orig_img)
+    if (verbose) {
+      message("# Resampling Image to 1x1x1")
+    }
+    img = resampleImage(img, c(1,1,1))
 
 
-  # Make all values positive, so 0s are 0s
-  img = img + 1025
-  lthresh = lthresh + 1025
+    # Make all values positive, so 0s are 0s
+    img = img + 1025
+    lthresh = lthresh + 1025
 
 
-  # Segmenting Lung and Airways (using code from segment_lung)
+    # Segmenting Lung and Airways (using code from segment_lung)
     if (verbose) {
       message("# Segmenting Lung and Airways: Thresholding")
     }
-    res = segment_human(
-      img = img,
-      adder = 0,
-      lthresh = lthresh,
-      verbose = TRUE
-    )
-    ebody = coarse_body(res$body)
-    newimg = mask_img(res$smoothed, ebody)
-    lung_air_mask = newimg < (lthresh + adder) & newimg > 0 & ebody == 1
+    body_img = img <= lthresh
 
+
+    # Find the lungs and trachea from the image
     if (verbose) {
       message("# Segmenting Lung and Airways: Largest Component")
     }
-    lung_air_mask = iMath(img = lunf_air_mask, operation = "GetLargestComponent")
+    first_img = iMath(img = body_img, operation = "GetLargestComponent")
 
-    if (verbose) {
-      message("# Segmenting Lung and Airways: Filling Holes")
+    # Check to see if we grabbed the background instead of the lung/trachea
+    # The back 10 slice and/or front 10 slices should have mean ~ 1, if selected
+    n_max = dim(first_img)[2]
+    mean_back = mean(apply(as.array(first_img)[,1:10,],2,mean))
+    mean_front = mean(apply(as.array(first_img)[,(n_max-9):n_max,],2,mean))
+
+    # Remove background, as necessary
+    if(mean_back > .5 | mean_front > .5)
+    {
+      first_img_hold = antsImageClone(first_img)
+
+      # Remove background from rest of image
+      first_img = (1-first_img_hold) * body_img
+      first_img = iMath(img = first_img, operation = "GetLargestComponent")
+
+      # Check to see if that fixed the problem
+      mean_back = mean(apply(as.array(first_img)[,1:10,],2,mean))
+      mean_front = mean(apply(as.array(first_img)[,(n_max-9):n_max,],2,mean))
+
+      # Keep fixing, as necessary
+      if(mean_back > .5 | mean_front > .5) # Still background
+      {
+        first_img_hold[first_img==1] = 1
+        first_img = (1-first_img_hold) * body_img
+        first_img = iMath(img = first_img, operation = "GetLargestComponent")
+
+        # Check to see if that fixed the problem
+        mean_back = mean(apply(as.array(first_img)[,1:10,],2,mean))
+        mean_front = mean(apply(as.array(first_img)[,(n_max-9):n_max,],2,mean))
+        if(mean_back > .5 | mean_front > .5){
+          first_img = segment_lung(first_img, lthresh = )
+          stop("Can't find lungs beneath background noise. More coding needed")
+          }
+
+      }
     }
-    lung_air_mask = filler(lung_air_mask, fill_size = 2)
+    lung_air_mask = iMath(first_img, "MC", 2)
 
 
-  # Segmenting Airways
+
+    # Segmenting Airways
     if (verbose) {
       message("# Segmenting Airways: Thresholding")
     }
@@ -79,7 +106,7 @@ segment_lung_lr = function(img, lthresh = -300, verbose = TRUE){
     air_mask = iMath(air_mask, "MD", 2)
 
 
-  # Segmenting Lung
+    # Segmenting Lung
     if (verbose) {
       message("# Segmenting Lung: Removing Airways from Lung")
     }
@@ -102,6 +129,12 @@ segment_lung_lr = function(img, lthresh = -300, verbose = TRUE){
     j = 0
     lung_mask2 = antsImageClone(lung_mask)
     while(n_clus == 2){
+      if(i == 0) {
+        lung_mask2 = iMath(lung_mask2, "ME", 1)
+        j = j + 1
+        left_right_mask = labelClusters(lung_mask2, minClusterSize = 50000)
+        n_clus = length(unique(left_right_mask))
+      }
       i = i + 1
       new_lthresh = lthresh - 50*i
       if(new_lthresh < 0){
@@ -115,14 +148,22 @@ segment_lung_lr = function(img, lthresh = -300, verbose = TRUE){
       img_mask = img_masked < new_lthresh
       lung_mask2[img_mask == 0] = 0
       left_right_mask = labelClusters(lung_mask2, minClusterSize = 50000)
-      n_clus = length(unique(left_right_mask))
+      num = unique(left_right_mask)
+      n_clus = length(num)
     }
 
     # Correct number of clusters
     if (n_clus == 3) {
 
+      # This was added in case the background is non-zero
+      left_right_mask = left_right_mask + 1
+      left_right_mask = maskImage(left_right_mask, lung_mask2)
+      num = unique(left_right_mask)
+      if(1 %in% num & 3 %in% num){left_right_mask[left_right_mask==3] <- 2}
+      if(2 %in% num & 3 %in% num){left_right_mask <- left_right_mask - 1}
+
       # Find coordinates of left and right clusters
-      coord = sapply(1:n_clus, function(i){
+      coord = sapply(1:3, function(i){
         img = left_right_mask == i
         loc = which(as.array(img) == 1, arr.ind = T)
         x = median(loc[,1])
@@ -141,7 +182,9 @@ segment_lung_lr = function(img, lthresh = -300, verbose = TRUE){
         message("# Segmenting Lung: Finishing Touches")
       }
       left_mask = iMath(left_mask, "MC", 8+2*j)
+      left_mask = iMath(img = left_mask, operation = "GetLargestComponent")
       right_mask = iMath(right_mask, "MC", 8+2*j)
+      right_mask = iMath(img = right_mask, operation = "GetLargestComponent")
       left_right_mask = right_mask + left_mask * 2
       left_right_mask[left_right_mask == 3] = 0
       left_right_mask[lung_air_mask == 0] = 0
@@ -149,14 +192,15 @@ segment_lung_lr = function(img, lthresh = -300, verbose = TRUE){
     } else{message("# Error: Too many clusters")}
 
 
-  if (verbose) {
-    message("# Resampling Back to Original Image Size")
-  }
-  left_right_mask = resampleImage(left_right_mask,
-                            resampleParams = dim(orig_img),
-                            useVoxels = TRUE,
-                            interpType = 1)
-  return(left_right_mask)
+    if (verbose) {
+      message("# Resampling Back to Original Image Size")
+    }
+    left_right_mask = resampleImage(left_right_mask,
+                                    resampleParams = dim(orig_img),
+                                    useVoxels = TRUE,
+                                    interpType = 1)
+    return(left_right_mask)
+
 
 }
 
